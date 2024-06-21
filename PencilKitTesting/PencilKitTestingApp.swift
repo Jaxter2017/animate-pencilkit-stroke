97237d592fcd390c1ab3c6c8a52abb73c99f7202
James <jamesbooth26@gmail.com>
2024-06-21 19:16:50 +0100

    Initial Commit

---
+++ PencilKitTesting/PencilKitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitTestingApp.swift
+//  PencilKitTesting
+//
+//  Created by James Booth on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PencilKitTestingTests/PencilKitTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitTestingTests.swift
+//  PencilKitTestingTests
+//
+//  Created by James Booth on 21/06/2024.
+//
+
+import Testing
+@testable import PencilKitTesting
+
+struct PencilKitTestingTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
