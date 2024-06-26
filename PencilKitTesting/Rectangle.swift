9f9080a2f6902ab36c30bd53613017f5854db264
James <jamesbooth26@gmail.com>
2024-06-26 11:35:27 +0100

    ye

---
+++ PencilKitTesting/Rectangle.swift
@@ -0,0 +1,18 @@
+//
+//  Rectangle.swift
+//  PencilKitTesting
+//
+//  Created by James Booth on 22/06/2024.
+//
+
+import SwiftUI
+
+struct Rectangle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Rectangle()
+}
